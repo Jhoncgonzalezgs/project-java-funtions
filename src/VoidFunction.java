9249ba63640c14277a73f5632002101efd01516a
Jhoncgonzalezgs <jhonc.gonzalezg@gmail.com>
2025-05-13 19:10:54 -0500

    Función sin parámetros y sin retorno implementada

--- src/VoidFunction.java
+++ src/VoidFunction.java
@@ -1,6 +1,6 @@
 public class VoidFunction {
     public static void main(String[] args) {
-        // TODO: Llamar a una función que no recibe ni devuelve nada
+        saludar(); // Llamamos la función
     }
 
     public static void saludar() {
