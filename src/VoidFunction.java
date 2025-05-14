f1239f5dd3bcb72b79c23050f148a2fd743de0a7
Jhoncgonzalezgs <jhonc.gonzalezg@gmail.com>
2025-05-13 19:08:48 -0500

    Estructura básica del proyecto Java

---
+++ src/VoidFunction.java
@@ -0,0 +1,9 @@
+public class VoidFunction {
+    public static void main(String[] args) {
+        // TODO: Llamar a una función que no recibe ni devuelve nada
+    }
+
+    public static void saludar() {
+        System.out.println("¡Hola, mundo!");
+    }
+}
